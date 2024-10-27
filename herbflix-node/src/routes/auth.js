dd41ee83437b414b89bcdc09b83a0c67f9bc4d30
Herbert Nícolas <herbert.nicolas@fabricadenegocio.com.br>
2024-10-27 15:17:09 -0300

    feat: added node structure and auth rout

---
+++ herbflix-node/src/routes/auth.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const User = require('../Schemas/User');
+const jwt = require('jsonwebtoken');
+
+const router = express.Router();
+
+router.post('/register', async (req, res) => {
+    try {
+        const user = new User(req.body);
+        await user.save();
+        res.status(201).send(user);
+    } catch (error) {
+        res
+            .status(400)
+            .send({ message: 'Error registering user', error });
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await User
+            .findOne({ username })
+            .exec();
+        if (!user) {
+            return res
+                .status(400)
+                .send({ message: 'The username does not exist' });
+        }
+        const valid = await user.comparePassword(password);
+        if (!valid) {
+            return res
+                .status(400)
+                .send({ message: 'The password is not correct' });
+        }
+        const token = jwt.sign({ _id: user._id }, process.env.SECRET);
+        res.status(200).send({ token });
+    } catch (error) {
+        res
+            .status(400)
+            .send({ message: 'Error logging in', error });
+    }
+});
+
+module.exports = router;
--- herbflix-node/src/server.js
+++ herbflix-node/src/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
-require('./mongo');
+require('./mongo').default;
+require('dotenv').config();
 
 const app = express();
+app.use(bodyParser.json());
+app.use('/auth', authRoutes);
 
 app.get('/api', (req, res) => {
     res.json({ message: 'Welcome' });
@@ -13,5 +16,3 @@ app.listen(process.env.API_PORT, () => {
     console.log(`Server is running on port ${process.env.API_PORT}`);
 });
 
-// app.use(bodyParser.json());
-// app.use('/auth', authRoutes);
